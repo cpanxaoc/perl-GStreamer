714af9fa662b3a24684e5f5f3beb061120d57b1b
Torsten Schönfeld <tsch@src.gnome.org>
2008-03-16 12:18:44 +0000

    GStreamer.pm NEWS README Release 0.10.
    	* GStreamer.pm
    	* NEWS
    	* README
    	* copyright.pod: Release 0.10.
    
    svn path=/trunk/; revision=69

--- GStreamer.pm
+++ GStreamer.pm
@@ -28,7 +28,7 @@ our @EXPORT_OK = qw(
 
 # --------------------------------------------------------------------------- #
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 sub import {
   my ($self) = @_;
@@ -299,6 +299,6 @@ on basic stuff like signals or object properties, this is what you should read.
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2006 by the gtk2-perl team
+Copyright (C) 2005-2008 by the gtk2-perl team
 
 =cut
